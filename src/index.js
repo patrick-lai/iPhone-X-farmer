fcb9ea6175ded9d1125515c2993869d9b6e14227
Patrick Lai <patrick.lai@lifeletters.com.au>
2016-10-20 15:25:49 +1100

    Use production port

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ app.get('/fetch', function(req, res, next) {
   res.json(appleFetcher.getStores(req.query.token));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
 });
 
 appleFetcher.fetchAllStores();
